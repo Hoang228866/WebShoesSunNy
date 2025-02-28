614e74d023a2d5b86371f79b0e9a01d00c3a575c
Admin <Hoangtmph13787@fpt.edu.vn>
2025-02-28 13:25:24 +0700

    no message

--- webshoes/src/main/java/com/shoes/webshoes/entity/Color.java
+++ webshoes/src/main/java/com/shoes/webshoes/entity/Color.java
@@ -1,2 +1,30 @@
-package com.shoes.webshoes.entity;public class Color {
+
+package com.shoes.webshoes.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = false)
+@Data
+@Entity
+@Table(name = "colors")
+public class Color extends BaseEntity{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private int status;
 }
