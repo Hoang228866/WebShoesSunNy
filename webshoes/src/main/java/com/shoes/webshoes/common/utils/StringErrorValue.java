9a271532208fb97cd9200895b02a0682cac7648d
Admin <Hoangtmph13787@fpt.edu.vn>
2025-03-03 21:42:52 +0700

    datn

--- webshoes/src/main/java/com/shoes/webshoes/common/utils/StringErrorValue.java
+++ webshoes/src/main/java/com/shoes/webshoes/common/utils/StringErrorValue.java
@@ -29,6 +29,8 @@ public class StringErrorValue {
 
 	public static final String SIZE_NOT_FOUND = "size không tồn tại!";
 	public static final String SIZE_IS_EXIST = "size đã tồn tại!";
-
-	
+	public static final String COLOR_NOT_FOUND = "màu sắc không tồn tại!";
+	public static final String COLOR_IS_EXIST = "màu sắc đã tồn tại!";
+	public static final String MATERIALS_NOT_FOUND = "chất liệu không tồn tại!";
+	public static final String MATERIALS_IS_EXIST = "chất liệu đã tồn tại!";
 }
